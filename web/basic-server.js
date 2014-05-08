cfa7287dd76997c44a3f812025234fb59b5bb7cc
Hack Reactor Students <adrice727@gmail.com>
2014-05-08 10:44:03 -0700

    Updated cronJob

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,5 +1,15 @@
 var http = require("http");
 var handler = require("./request-handler");
+var cronJob = require("cron").CronJob;
+var archive = require("../workers/htmlfetcher").archive;
+
+new cronJob({
+  cronTime: '* * * * *',
+  onTick: function() {
+    archive();
+  },
+  start: true
+});
 
 var port = 8080;
 var ip = "127.0.0.1";
--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,10 +1,11 @@
 var archive = require("../helpers/archive-helpers");
 
-archive.readListOfUrls(function(list) {
-  for (var i = 0; i < list.length; i++) {
-    archive.isURLArchived(list[i], function(url, isArchived) {
-      console.log(url);
-      isArchived || archive.downloadUrl(url);
-    });
-  }
-});
+exports.archive = function() {
+  archive.readListOfUrls(function(list) {
+    for (var i = 0; i < list.length; i++) {
+      archive.isURLArchived(list[i], function(url, isArchived) {
+        isArchived || archive.downloadUrl(url);
+      });
+    }
+  });
+};
